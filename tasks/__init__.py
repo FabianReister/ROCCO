9ab76e57835595e2b744e1ef28d37f2218b20273
Fabian Reister <fabian.reister92@gmail.com>
2018-09-21 13:01:20 +0200

    implemented evaluation task. this task currently works up to the line 'from thirdparty.cityscapesscripts.evaluation.evalPixelLevelSemanticLabeling import evaluateImgLists'

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -3,5 +3,6 @@ from .task import Task
 # import all subclasses
 from .train import TaskTrain
 from .predict import TaskPredict
+from .evaluate import TaskEvaluate
 
 TASKS = dict([(task.name(), task) for task in Task.__subclasses__()])
---
+++ tasks/evaluate.py
@@ -0,0 +1,77 @@
+from .task import Task
+
+from utils.train_data_producer import TrainDataProducer, ValidDataProducer
+from utils.augmenter import Augmenter
+from utils.preprocessor import Preprocessor
+from datasets import DATASETS
+
+from cv2 import imread, imwrite, resize, INTER_NEAREST
+import numpy as np
+import os.path
+
+class TaskEvaluate(Task):
+    def __init__(self, config):
+        self._config = config
+
+    @staticmethod
+    def name():
+        return "evaluate"
+
+    def trainLabelToEvalFilename(self, label_filename, eval_dir):
+        # /foo/bar/datasets/cityscapes/gtFine/test/aachen/aachen_000000_000019_gtFine_labelIds.png
+
+        # expect the train image to have a test parent folder
+        idx = label_filename.find('test')
+
+        assert idx is not -1
+
+        idx += 4 + 1  # length of 'test' + 1
+
+        relative_label_filename = label_filename[idx:]
+
+        return os.path.join(eval_dir, relative_label_filename)
+
+    def run(self, model):
+        test_dataset = DATASETS[self._config["dataset"]["name"]](self._config["dataset"], 'test', loop=False)
+
+        preprocessor = Preprocessor(self._config["preprocessor"])
+
+        gt_filenames = []
+        prediction_filenames = []
+
+        for img_filename, label_filename in test_dataset:
+            prediction_filename = self.trainLabelToEvalFilename(label_filename, self._config["eval_dir"])
+
+            # only run prediction if prediction image does not exist yet
+            if not os.path.exists(prediction_filename):
+
+                prediction_dir = os.path.dirname(prediction_filename)
+                if not os.path.exists(prediction_dir):
+                    os.makedirs(prediction_dir)
+
+                img = imread(img_filename)
+
+                assert img is not None
+
+                # feed the image through the network
+                x = preprocessor.process(img)
+                y_pred = model.predict(np.asarray([x])).squeeze()
+                y_label_pred = np.argmax(y_pred, axis=2)
+                y_label_pred = np.asarray(y_label_pred, dtype=np.uint8)
+
+                y_label_pred = resize(y_label_pred, (img.shape[1], img.shape[0]), interpolation=INTER_NEAREST)
+
+                # store it in the eval folder
+                imwrite(prediction_filename, y_label_pred)
+
+            gt_filenames.append(label_filename)
+            prediction_filenames.append(prediction_filename)
+
+        # finally run evaluation
+        from thirdparty.cityscapesscripts.evaluation.evalPixelLevelSemanticLabeling import evaluateImgLists
+        args = {
+            'quiet': True
+        }
+        confusion_matrix = evaluateImgLists(prediction_filenames, gt_filenames, args)
+
+        np.savetxt("{}/confusion_matrix.csv".format(self._config["eval_dir"]), confusion_matrix)

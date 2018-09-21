3563ed4e654e324eca181c544021fe93756bc650
Fabian Reister <fabian.reister92@gmail.com>
2018-09-21 13:00:08 +0200

    updated dataset classes

--- datasets/cityscapes.py
+++ datasets/cityscapes.py
@@ -2,10 +2,27 @@ from .dataset import Dataset
 
 
 class Cityscapes(Dataset):
-    def __init__(self, config):
-        self._config = config
+    def __init__(self, config, type, loop=True):
+        Dataset.__init__(self, loop)
+        dataset_path = config["path"]
+
+        img_paths = "{}/leftImg8bit/{}/*/*.png".format(dataset_path, type)
+
+        from glob2 import glob
+        img_files = glob(img_paths)
+
+        label_files = map(self.img_to_label_filename, img_files)
+
+        self._dataset = zip(img_files, label_files)
 
     @staticmethod
     def name():
         return "cityscapes"
+
+    @staticmethod
+    def img_to_label_filename(img_filename):
+        img_filename = img_filename.replace('/leftImg8bit/', '/gtFine/')
+        img_filename = img_filename.replace('leftImg8bit', 'gtFine_labelTrainIds')
+        return img_filename
+
 
--- datasets/dataset.py
+++ datasets/dataset.py
@@ -2,13 +2,29 @@ from abc import abstractmethod
 
 
 class Dataset(object):
+    _dataset = []
+    _idx = -1
+
+    def __init__(self, loop):
+        self.__loop = loop
+
+    def __iter__(self):
+        return self
 
     @staticmethod
     @abstractmethod
     def name():
         pass
 
-    @abstractmethod
     def next(self):
-        pass
+        self._idx += 1
+
+        # reset if required
+        if self._idx >= len(self._dataset):
+            if self.__loop is True:
+                self._idx = 0
+            else:
+                raise StopIteration()
+
+        return self._dataset[self._idx]
 

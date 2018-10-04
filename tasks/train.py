6556ce5257e33e679d18625a62ae7c95d4ce69aa
Fabian Reister <fabian.reister92@gmail.com>
2018-10-04 13:08:18 +0200

    task train: no hard coded optimizer, saving model in log dir, auto num samples per epoch based on dataset and batch size

--- tasks/train.py
+++ tasks/train.py
@@ -15,25 +15,44 @@ class TaskTrain(Task):
     def name():
         return "train"
 
-    def run(self, model):
+    def run(self, model, log_dir):
+
+
         train_dataset = DATASETS[self._config["dataset"]["name"]](self._config["dataset"], 'train')
-        valid_dataset = DATASETS[self._config["dataset"]["name"]](self._config["dataset"], 'val')
+        val_dataset = DATASETS[self._config["dataset"]["name"]](self._config["dataset"], 'val')
 
         augmenter = Augmenter(self._config["augmenter"])
         preprocessor = Preprocessor(self._config["preprocessor"])
 
         from keras.callbacks import TensorBoard
-        import datetime
-        tensor_board = TensorBoard(log_dir="./logs/{}".format(datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")))
+        tensor_board = TensorBoard(log_dir=log_dir)
 
-        model.compile(optimizer="Adam", loss="categorical_crossentropy", metrics=["categorical_accuracy"])
+        from keras.optimizers import get as get_optimizer
+        optimizer = get_optimizer(self._config["optimizer"])
+
+        model.compile(optimizer=optimizer, loss="categorical_crossentropy", metrics=["categorical_accuracy"])
 
         train_data_producer = TrainDataProducer(config=self._config, dataset=train_dataset, augmenter=augmenter,
                                                 preprocessor=preprocessor)
-        valid_data_producer = ValidDataProducer(config=self._config, dataset=valid_dataset, preprocessor=preprocessor)
+        valid_data_producer = ValidDataProducer(config=self._config, dataset=val_dataset, preprocessor=preprocessor)
 
-        model.fit_generator(generator=train_data_producer, steps_per_epoch=self._config["steps_per_epoch"],
-                            validation_data=valid_data_producer, validation_steps=self._config["validation_steps"],
-                            epochs=self._config["epochs"], callbacks=[tensor_board])
+        train_steps_per_epoch = train_dataset.num_samples() / self._config["batch_size"]
+        val_steps_per_epoch = val_dataset.num_samples() / self._config["batch_size"]
 
-        model.save('frrn.h5')
+        model.fit_generator(generator=train_data_producer,
+                            steps_per_epoch=train_steps_per_epoch,
+                            validation_data=valid_data_producer,
+                            validation_steps=val_steps_per_epoch,
+                            epochs=self._config["epochs"],
+                            callbacks=[tensor_board])
+
+        # save the model in the log directory
+        import os
+        trained_model_filename = os.path.join(log_dir, 'trained_model.h5')
+
+        print("Saving trained model to %s" % trained_model_filename)
+
+        # If the saving does not work, take a look at
+        # https://github.com/keras-team/keras/issues/6766
+        # and then upgrade keras!
+        model.save(trained_model_filename)
